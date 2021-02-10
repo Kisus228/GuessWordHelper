637efe12b166eef54bea48c899e5277545b2f704
Kisus228 <nik.babushkin.201408@gmail.com>
2021-02-10 20:02:55 +0500

    Добавлена поддержка шаблона с числами

--- FindWordCommand.cs
+++ FindWordCommand.cs
@@ -10,15 +10,21 @@ namespace GuessWordHelper
     {
         [Value(0)] public string WordToGuess { get; set; }
 
+        private static readonly Regex RegexPattern = new Regex(@"(?<count>\d+)_");
+
         public static void FindWords(FindWordCommand opts, HashSet<string> wordsDic)
         {
-            var lettersCount = opts.WordToGuess.Length;
-            opts.WordToGuess = opts.WordToGuess
+            foreach (Match match in RegexPattern.Matches(opts.WordToGuess))
+            {
+                opts.WordToGuess =
+                    opts.WordToGuess.Replace(match.Value, @"\w{" + $"{match.Groups["count"].Value}" + @"}");
+            }
+            opts.WordToGuess = @"\A" + opts.WordToGuess
                 .Replace("_", @"\w")
-                .ToLower();
+                .ToLower() + @"\Z";
             var regex = new Regex(opts.WordToGuess);
             var matches = wordsDic
-                .Where(word => word.Length == lettersCount && regex.Match(word).Success)
+                .Where(word => regex.Match(word).Success)
                 .ToHashSet();
             PrintHandler.PrintMatches(matches);
         }
